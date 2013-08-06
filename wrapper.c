7d380ee8a061e53332fdf237b5d226614f3f7516
FacundoAcevedo <acv2facundo@gmail.com>
2013-08-06 10:42:27 -0300

    Comentado el codigo, y un poquito de refactor

--- wrapper.c
+++ wrapper.c
@@ -1,19 +1,21 @@
+
+
+//#####################################################
+#define RUTA_CONFIG "/etc/samba/gruposhabilitados.txt"
+//#####################################################
+
 #define _GNU_SOURCE
 #define MAXGRUPOS 30
 #define dim(x) (sizeof(x)/sizeof(x[0]))
 #define true 1
 #define false 0
-
-#define RUTA_CONFIG "/etc/samba/gruposhabilitados.txt"
-
-
-
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 //
 //Me permite llamar al metodo original
 #include <dlfcn.h> 
+//codigos de error
 #include <errno.h>
 
 //mkdir()
@@ -21,10 +23,9 @@
 #include <sys/types.h> 
 
 //para obtener los usuarios
-#include <pwd.h>
 #include <unistd.h>
 
-//parser
+//parser de la config
 #include <libconfig.h>
 
 /* "disimula" al syscall mkdir(), primero verifica que quien intenta crear el
@@ -32,34 +33,34 @@
  * si pertenece, le permite crear el directorio, de lo contrario dara error de permisos
  * */
 
-/* si grupoUsuario en gruposAutorizados:
- * 	mkdirOriginal()
- * sino:
- * 	return ERRORPERMISOS
- */
-
 int *obtenerGruposValidos(){
 	//Hardcodeo la ruta
 	
 	int  indice = 1;
-	int (*gruposValidos) = malloc(sizeof (int) * MAXGRUPOS); //Tambien hardcodeada
+	//Reservo el espacio para los grupos validos
+	int (*gruposValidos) = malloc(sizeof (int) * MAXGRUPOS); 
 
         config_t cfg, *cf;
         const config_setting_t *grupos;
         int count, n;
 	
 	//agrego a root a la lista
-	gruposValidos[0]=0L;
+	gruposValidos[0]=0;
     
+	//Inicializo el parser de configuracion
         cf = &cfg;
         config_init(cf);
     
+	//Intento leer el archivo
         if (!config_read_file(cf, RUTA_CONFIG )) {
            puts("ERROR al parsear\n");
-            config_destroy(cf);
+           config_destroy(cf);
+	   return gruposValidos;
         }
 	
+	//Obtengo los grupos parseados
 	grupos = config_lookup(cf, "habilitadosCarpetas.gruposHabilitados");
+	//obtengo la cantidad de grupos
         count = config_setting_length(grupos);
     
         for (n = 0; n < count; n++) {
@@ -68,10 +69,11 @@ int *obtenerGruposValidos(){
                 gruposValidos[n+1] = ((int) config_setting_get_int_elem(grupos, n));
         }   
     
+	//Libero la memoria de la configuracion
         config_destroy(cf);
 
 
-	//cargo -1 en el resto de los casilleros vacios
+	//cargo -1 en el resto de los casilleros vacios, para que n tengan mugre
 	indice = n+1;
 	while ( indice < MAXGRUPOS  )
 		gruposValidos[indice++] = -1;
@@ -79,37 +81,35 @@ int *obtenerGruposValidos(){
 	return gruposValidos;
 
 
-}
+}//obtenerGruposValidos
+
+
 int verificarGrupos(void){
   gid_t gids[MAXGRUPOS];
-
   int *gruposValidos = obtenerGruposValidos();
-  
   int count, curr, aux;
+  
   //Obtengo la cantidad de grupos en count, y guardo los gid en gids.
-  if ((count = getgroups(dim(gids), gids)) == -1)
-
+  if ((count = getgroups(dim(gids), gids)) == -1){
     perror("getgroups() error");
+    return false;
+  }
+
 
   else {
-	
-    fflush(NULL);
-    for (curr=0; curr<count; curr++) {
+    fflush(NULL);//me tiraba un error sin esto
+    for (curr=0; curr<count; curr++){
 	for(aux=0; aux < MAXGRUPOS; aux++){
-              printf("%i-%i) Guid: %d - grupoValido: %d\n",curr,aux,((int) gids[curr]) ,(int) ((gruposValidos)[aux]));
+//              printf("%i-%i) Guid: %d - grupoValido: %d\n",curr,aux,((int) gids[curr]) ,(int) ((gruposValidos)[aux]));
+	      //Valido que el grupo del usuario sea uno valido
 	      if  (((int) gids[curr]) == ((int) ((gruposValidos)[aux]) )){
-		      
 		free(gruposValidos);
-		
-		puts("Habilitado");
-
 		return true;
 		}//if
-    }//for
+    	}//for
 
     }//for
 
-    puts("NO Habilitado");
     free(gruposValidos);
     return false;
   }//else
@@ -118,19 +118,24 @@ int verificarGrupos(void){
 int mkdir(const char *pathname, mode_t mode){
   int habilitadoAEscribir = false;
   
+  //LLamo a la verificacion de grupos
   habilitadoAEscribir = verificarGrupos();
   
   if ( habilitadoAEscribir == true ){
-
+	//"instancio" al mkdir original
   	int (*mkdir_real)(const char *pathname, mode_t mode);
   	mkdir_real = dlsym(RTLD_NEXT,"mkdir");
+	//y lo ejecuto
 	mkdir_real(pathname, mode);
 	return 0;
 
   }
-   else
-	errno = EPERM; 
-   	return -1;
+   else{
+	//errno es una variable en errno.h que va a ser usada por perror,
+	//le avisa al sistema que error mostrar, en este caso por falta de privilegios
+	errno=EPERM;
+   	return(errno);
+   }
 
 }
 int rmdir(const char *pathname){
@@ -146,8 +151,8 @@ int rmdir(const char *pathname){
 	return 0;
 
   }
-   else
-	errno = EPERM; 
-   	return -1;
-
+   else{
+	errno=EPERM;
+   	return(errno);
+       }
 }
