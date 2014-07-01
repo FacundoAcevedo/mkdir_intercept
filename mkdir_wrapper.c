31ece02174059aad0c50e86f34e4cecc6a8bb7f5
Facundo Acevedo <acv2Facundo@gmail.com>
2014-07-01 13:49:49 -0300

    rediseñada la funcion directorioAfectado

--- mkdir_wrapper.c
+++ mkdir_wrapper.c
@@ -1,7 +1,8 @@
 
 
 //#####################################################
-#define RUTA_CONFIG "/etc/samba/gruposhabilitados.txt"
+//#define RUTA_CONFIG "/etc/samba/gruposhabilitados.txt"
+#define RUTA_CONFIG "gruposhabilitados.txt"
 //#####################################################
 
 #define DEBUG 1
@@ -38,6 +39,7 @@
 
 #include <time.h>
 
+#include "logger.h"
 
 /* "disimula" al syscall mkdir(), primero verifica que quien intenta crear el
  * directorio sea un usuario que pertenezca a alguno de los grupos autorizados.
@@ -46,21 +48,47 @@
  * */
 
 ////////////////////< FIRMAS
+//Verifica si el usuario esta habilitado a escribir
 bool habilitadoAEscribir(const char*);
 int *obtenerGruposValidos( config_t*);
 bool afectaAlDirectorio(config_t* , const char*);
 bool verificarGrupos(config_t*);
-bool logger(char*);
 ///////////////////< FUNCIONES
 
 
+
+//Hace todas las verificaciones
+bool verificar_habilitacion(const char *pathname){
+	if (DEBUG){
+        LOG_PRINT("-verificar_habilitacion()");
+    }
+
+	config_t cfg, *cf;
+
+    //Inicializo el parser de configuracion
+    cf = &cfg;
+    config_init(cf);
+    
+	//Intento leer el archivo
+    if (!config_read_file(cf, RUTA_CONFIG )) {
+       puts("ERROR al parsear la configuracion\n");
+       if (DEBUG)
+           LOG_PRINT("ERROR al parsear la configuracion");
+       config_destroy(cf);
+       return false;
+    }
+
+    //Obtengo los directorios afectados
+
+
+}
 bool habilitadoAEscribir(const char *pathname){
 	//Corre todas las comprobaciones necesarias para
 	//determinar si el usuario puede crear un directorio ( o eleminarlo)
 	
 	if (DEBUG){
 		puts("-HabilitadoAEscribir()\n");
-        logger("-HabilitadoAEscribir()");
+        LOG_PRINT("-HabilitadoAEscribir()");
     }
 
 
@@ -75,7 +103,7 @@ bool habilitadoAEscribir(const char *pathname){
         if (!config_read_file(cf, RUTA_CONFIG )) {
            puts("ERROR al parsear la configuracion\n");
            if (DEBUG)
-               logger("ERROR al parsear la configuracion");
+               LOG_PRINT("ERROR al parsear la configuracion");
            config_destroy(cf);
            return false;
         }
@@ -84,9 +112,7 @@ bool habilitadoAEscribir(const char *pathname){
 	if(config_lookup_bool(cf, "Carpetas.activo", &activo)){
 		if (DEBUG){
 			printf("-Activo:%d\n",activo);
-            char msg[100];
-            sprintf(msg, "-Activo: %d\n",activo);
-            logger(msg);
+            LOG_PRINT("-Activo: %d",activo);
         }
 		if( activo == 0 ){
 			   config_destroy(cf);
@@ -96,8 +122,10 @@ bool habilitadoAEscribir(const char *pathname){
 			if(afectaAlDirectorio(cf, pathname)){
 				//El directorio es afectado por la configuracion
 
-				if (DEBUG)
+				if (DEBUG){
 					printf("-AFECTA AL DIRECTORIO\n");
+                    LOG_PRINT("-AFECTA AL DIRECTORIO");
+                }
 
 				if ( verificarGrupos(cf)){
         				config_destroy(cf);
@@ -112,7 +140,8 @@ bool habilitadoAEscribir(const char *pathname){
 			}//if
 			else{
 				if (DEBUG)
-					printf("-NO AFECTA AL DIRECTORIO\n");
+				   printf("-NO AFECTA AL DIRECTORIO\n");
+                   LOG_PRINT("No afecta al directorio");
 				   config_destroy(cf);
 				   return true;
 			}//else
@@ -134,8 +163,10 @@ int *obtenerGruposValidos( config_t* cf){
 	 * que estan habilitados para crear o eliminar directorios*/
 
 	
-				if (DEBUG)
+				if (DEBUG){
 					printf("---obtenerGruposValidos\n");
+                    LOG_PRINT("---ObtenerGruposValidos");
+                    }
 	int  indice = 1;
 	//Reservo el espacio para los grupos validos
 	int (*gruposValidos) = malloc(sizeof (int) * MAXGRUPOS); 
@@ -154,6 +185,7 @@ int *obtenerGruposValidos( config_t* cf){
     
 	for (n = 0; n < count; n++) {
 		printf("Grupo: %d\n", (int) config_setting_get_int_elem(grupos, n));
+        LOG_PRINT("Grupo: %d", (int) config_setting_get_int_elem(grupos, n));
 		//Agrego los grupos del archivo
 		gruposValidos[n+1] = ((int) config_setting_get_int_elem(grupos, n));
 	}//for
@@ -169,77 +201,145 @@ int *obtenerGruposValidos( config_t* cf){
 
 }//obtenerGruposValidos
 
-
-bool afectaAlDirectorio(config_t *cf, const char *directorioActual){
-	//Verifica que el directorio se vea afectado
-	
-	if (DEBUG)
-		printf("--afectaAlDirectorio()\n");
-
-	const config_setting_t *directoriosAfectados;
+//Rediseñada
+//Verifica si el directorio esta afectado por la configuracion
+//de estar afectado devolvera la ruta base de la configuracion
+//EJ: dir afectado: /a/b; dir_a_crear: /a/b/c
+//devolvera /a/b
+//EJ: dir_a_crear: /a/d
+//devolvera: NULL
+char* directorioAfectado(config_t *cf, const char *directorio_verificar)
+{
+	const config_setting_t *directorios_afectados;
 	int cantidad=0, afectado=true;
+    char* directorio_posible = NULL;
 	
-	directoriosAfectados = config_lookup(cf, "Carpetas.rutasAfectadas");
-	cantidad = config_setting_length(directoriosAfectados);
+    //Obtengo la lista de directorios afectados
+	directorios_afectados = config_lookup(cf, "rutasAfectadas");
+    //Cantidad de directorios afectados
+	cantidad_directorios = config_setting_length(directoriosAfectados);
 
-				if (DEBUG)
-					printf("--Directorio actual: %s \n",directorioActual);
-	for (int n = 0; n < cantidad; n++) {
-		char* directorioAfectado = ((char*) config_setting_get_string_elem(directoriosAfectados,n));
+    //Recorro la lista de directorios afectados
+    for (int i = 0; i < cantidad_directorios; i++)
+    {
+        //Obtengo la ruta del directorio afectado numero i
+        char* directorioAfectado = ((char*) config_setting_get_string_elem(directorios_afectados, i));
+        
+        //Comparo los largos
+		int comparacion = strcmp(directorio_verificar, directorio_afectado);
 
-				if (DEBUG)
-					printf("--Directorio afectado: %s \n",directorioAfectado);
-
-		//Comparo el directorio actual con los directorios afectados)
-		int comparacion = strcmp(directorioActual, directorioAfectado);
-
-				if (DEBUG)
-					printf("--Comparacion: %i \n",comparacion);
-
-		//Obtengo la posicion de la subtring
-		char* posicionSubString = strstr(directorioActual, directorioAfectado);
-		char* posicionSubStringInverso = strstr( directorioAfectado, directorioActual);
-
-		if(posicionSubString == NULL && posicionSubStringInverso == NULL){
-
-				if (DEBUG)
-					printf("--No son substrings\n");
+        //verificar en afectado
+		char* posicionSubString = strstr( directorio_afectado, directorio_verificar);
+        //afectado en verificar
+		char* posicionSubStringInverso = strstr(directorio_verificar, directorio_afectado);
 
 
-			afectado = false; //Significa que el directorio actual no esta debajo de nungun directorio prohibido
-		}//if
-		else{
-			//la ruta actual es substring de alguna ruta afectada
-			if( comparacion >=0){ //La ruta actual es mas larga ( o igual)  que la ruta afectada
-				if (DEBUG)
-					printf("--MAYO O IGUAL\n");
-				return  true;
-				}//if
-			else //La ruta actual es mas corta que sla ruta afectada
-				if (DEBUG)
-					printf("--MENOR\n");
-				afectado = false;
-
-		}//else
-	}//for
-				if (DEBUG)
-					printf("--afectado:%d \n",afectado);
-
-	return afectado;
-
-	
+		if(posicionSubString != NULL && posicionSubStringInverso == NULL){
+            //dir a verificar es un subdirectorio del directorio afectado
+            if ( directorio_posible != NULL){
+                if (strlen(directorio_afectado) < strlen(directorio_posible))
+                {
+                    //Voy asignandole la ruta mas cercana a la carpeta interesada
+                    directorio_posible = directorio_afectado;
+                }
+                //else -> mantendra la ruta mas cercana
+            }else{
+                directorio_posible = directorio_afectado;
 
 
+        }
+        else if (posicionSubString == NULL && posicionSubStringInverso != NULL){
+            //dir afectado es un subdirectorio del directorio a verificar 
+            //entonces no hago nada
+            continue;
+        }
+        else if (posicionSubString == NULL && posicionSubStringInverso == NULL){
+            //No hay ninguna coincidencia
+            continue;
+        }
+        else if (*posicionSubString == *posicionSubStringInverso){
+            //Son la misma ruta, asi que dejo de verificar y devuelvo la ruta
+            return directorio_verificar;
+        }
+        else
+            LOG_PRINT("Error al procesar las rutas.");
+    }//for
+    return directorio_posible;
+}
 
-}//verificarDirectorio
+
+
+//bool afectaAlDirectorio(config_t *cf, const char *directorioActual){
+//	//Verifica que el directorio se vea afectado
+//
+//	const config_setting_t *directoriosAfectados;
+//	int cantidad=0, afectado=true;
+//	
+//	directoriosAfectados = config_lookup(cf, "rutasAfectadas");
+//	cantidad = config_setting_length(directoriosAfectados);
+//
+//				if (DEBUG){
+//					printf("--Directorio actual: %s \n",directorioActual);
+//                    LOG_PRINT("--Directorio actual: %s ",directorioActual);
+//                }
+//
+//	for (int n = 0; n < cantidad; n++) {
+//        //Obtengo el elemento n de la lista de directorios afectados
+//		char* directorioAfectado = ((char*) config_setting_get_string_elem(directoriosAfectados,n));
+//
+//		//Comparo el directorio actual con los directorios afectados)
+//        //strcmp me devuelve la posicion donde comienza la coincidencia
+//		int comparacion = strcmp(directorioActual, directorioAfectado);
+//
+//				if (DEBUG){
+//					printf("--Comparacion: %i \n",comparacion);
+//                    LOG_PRINT("--Comparacion: %i",comparacion);  
+//                }
+//
+//		//Obtengo la posicion de la subtring
+//		char* posicionSubString = strstr(directorioActual, directorioAfectado);
+//		char* posicionSubStringInverso = strstr( directorioAfectado, directorioActual);
+//
+//		if(posicionSubString == NULL && posicionSubStringInverso == NULL){
+//
+//				if (DEBUG){
+//					printf("--No son substrings\n");
+//                    LOG_PRINT("--No son substrings");
+//                }
+//
+//
+//			afectado = false; //El directorio actual no esta debajo de nungun directorio prohibido
+//		}//if
+//
+//		else{
+//            
+//			//la ruta actual es substring de alguna ruta afectada
+//			if( comparacion >=0){ //La ruta actual es mas larga ( o igual)  que la ruta afectada
+//				return  true;
+//				}
+//
+//			else //La ruta actual es mas corta que sla ruta afectada
+//				afectado = false;
+//
+//		}//else
+//	}//for
+//	return afectado;
+//
+//	
+//
+//
+//
+//}//verificarDirectorio
 
 
 bool verificarGrupos(config_t* cf){
   gid_t gids[MAXGRUPOS];
   int *gruposValidos = obtenerGruposValidos(cf);
   int count, curr, aux;
-				if (DEBUG)
+				if (DEBUG){
 					printf("--verificarGrupos()\n");
+                    LOG_PRINT("--verificarGrupos()");
+                }
   
   //Obtengo la cantidad de grupos en count, y guardo los gid en gids.
   if ((count = getgroups(dim(gids), gids)) == -1){
@@ -270,14 +370,18 @@ bool verificarGrupos(config_t* cf){
 
 int mkdir(const char *pathname, mode_t mode){
 
-  if (strncmp(pathname, "foo", 3) == 0)
+ // habilitadoAEscribir(pathname);
+  //if (strncmp(pathname, "/tmp/foo",8) == 0)
+  if(strstr(pathname, "foo") != NULL)
   {
   	puts("Si es foo, deberias ver esto y no te crearia la carpeta");
-    logger("Prohibido crear foo");  
-  	return 0;
+    LOG_PRINT("No creo foo");
+	errno=EPERM;
+   	return(errno);
 
   } else {
   	
+    LOG_PRINT("Creo %s", pathname);
   	int (*mkdir_real)(const char *pathname, mode_t mode);
   	mkdir_real = dlsym(RTLD_NEXT,"mkdir");
 	mkdir_real(pathname, mode);
@@ -308,8 +412,10 @@ int mkdir(const char *pathname, mode_t mode){
 int rmdir(const char *pathname){
   bool habilitado = habilitadoAEscribir(pathname);
   
-  if (DEBUG)
+  if (DEBUG){
 	  puts("rmdir\n");
+      LOG_PRINT("rmdir"); 
+  }
   
   if ( habilitado == true ){
 
@@ -325,27 +431,3 @@ int rmdir(const char *pathname){
        }
 }
 
-//Funcion auxiliar de loggeo
-bool logger(char* inBuf) // inBuf = message to log
-{
-    time_t myTime = time(NULL);
-    char* fecha = ctime(&myTime);
-
-    char* caracter = strrchr(fecha, '\n');
-    if (caracter) *caracter='\0';
-
-    
-    FILE* fp = fopen("/tmp/mkdir.log", "a"); //File to open. "a" for append, or write to end of file
-    if(!fp) return false;
-    
-    //char* buf = (char*) malloc(sizeof(char)); //get space for the buffer
-    //char* buf = (char*) calloc(strlen(inBuf) + sizeof(myTime), sizeof(char)); //get space for the buffer
-    char* buf = (char*) malloc((strlen(inBuf) + strlen(fecha)) * sizeof(char)); //get space for the buffer
-    sprintf(buf, "%s - %s \n", fecha, inBuf); //format 'inBuf' 
-    //sprintf(buf, "%s - %s \n",ctime(&myTime),inBuf); //format 'inBuf' 
-    fprintf(fp, buf); //Write to the file
-    
-    fclose(fp);
-    //free(buf++);
-    return true;
-}
